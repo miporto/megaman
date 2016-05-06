be985c2914bc5bf1fdd3ddaabbf9b3f8c806ac1f
mporto <manu.porto94@gmail.com>
2016-05-06 20:19:43 -0300

    Update main.cpp

--- client/main.cpp
+++ client/main.cpp
@@ -1,15 +1,23 @@
 #include <cstdlib>
+#include <gtkmm/application.h>
 
 #include "common/Socket.h"
 #include "Client.h"
+#include "MainWindow.h"
 
 #define HOSTNAME_POS 1
 #define PORT_POS 2
 
 int main(int argc, char** argv) {
-    if (argc != 3) return 1;
-	Socket skt;
-	Client client(skt, argv[HOSTNAME_POS], argv[PORT_POS]);
-	client();
-	return EXIT_SUCCESS;
+    //if (argc != 3){return 1;}
+    Glib::RefPtr<Gtk::Application> app =
+        Gtk::Application::create(argc, argv, "org.gtkmm.example");
+
+    MainWindow mainwindow;
+
+    // Shows the window and returns when it is closed.
+    return app->run(mainwindow);
+    //Socket skt;
+    //Client client(skt, argv[HOSTNAME_POS], argv[PORT_POS]);
+    //client();
 }
