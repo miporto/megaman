aa6522db479bbc0b7d3ce8035df8919a7d95c5f7
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-05-18 15:40:12 -0300

    MegaMan class with EnergyTank and Cannon

--- server/MegaMan.cpp
+++ server/MegaMan.cpp
@@ -1,7 +1,29 @@
 #include "MegaMan.h"
 
+#define AMMO_KINDS 6
+
+// A reemplazar por .JSON
+#define AMMO_MAX 330
+#define AMMO_VEL 5 // Cada tipo de ammo tiene vel diferente
+#define AMMO_DAMAGE 5 // Cada tipo de ammo tiene damage diferente
 #define MAX_ENERGY 50
 #define INITIAL_LIVES 3
+//
+
+Ammo::Ammo() : max(AMMO_MAX),
+               damage(AMMO_DAMAGE),
+               vel(AMMO_VEL),
+               quantity(AMMO_MAX) {}
+
+void Ammo::use() {
+
+}
+
+Ammo::~Ammo() {}
+
+Cannon::Cannon() : ammos(AMMO_KINDS) {}
+
+Cannon::~Cannon() {}
 
 EnergyTank::EnergyTank() : lives(INITIAL_LIVES),
                            max_energy(MAX_ENERGY),
@@ -12,10 +34,24 @@ void EnergyTank::increase_energy(int amount) {
 }
 
 void EnergyTank::decrease_energy(int amount) {
-    if (current_energy < amount && )
+    if (this->current_energy < amount && this->lives > 0) {
+        amount -= this->current_energy;
+        this->reset();
+        this->current_energy -= amount;
+    } else if (this->current_energy < amount && this->lives == 0) {
+        this->current_energy = 0;
+    } else {
+        this->current_energy -= amount;
+    }
+}
 
+bool EnergyTank::is_empty() {
+    return this->current_energy <= 0 && lives <= 0;
+}
 
-    this->current_energy -= amount;
+void EnergyTank::reset() {
+    this->lives--;
+    this->current_energy = this->max_energy;
 }
 
 EnergyTank::~EnergyTank() {}
--- server/MegaMan.h
+++ server/MegaMan.h
@@ -1,8 +1,28 @@
 #ifndef MEGAMAN_H
 #define MEGAMAN_H
 
-class Cannon {
+#include <vector>
 
+class Ammo {
+private:
+    const int max;
+    const int damage;
+    const int vel;
+    int quantity;
+
+public:
+    Ammo();
+    void use();
+    ~Ammo();
+};
+
+class Cannon {
+    private:
+        std::vector<Ammo> ammos;
+
+    public:
+        Cannon();
+        ~Cannon();
 };
 
 class EnergyTank {
@@ -10,20 +30,25 @@ class EnergyTank {
         int lives;
         const int max_energy;
         int current_energy;
+
+        void reset();
+
     public:
         EnergyTank();
         void increase_energy(int amount);
         void decrease_energy(int amount);
+        bool is_empty();
         ~EnergyTank();
 };
 
 class MegaMan {
-private:
-    EnergyTank tank;
-    Cannon cannon;
-public:
-    MegaMan();
-    ~MegaMan();
+    private:
+        EnergyTank tank;
+        Cannon cannon;
+
+    public:
+        MegaMan();
+        ~MegaMan();
 };
 
 
