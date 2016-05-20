3b88d70a3c7f16e97482e08bbc57941be8634c38
mporto <mporto@fi.uba.ar>
2016-05-20 14:25:40 -0300

    Constrouctor now receives a pointer to MainWindow. Minor changes

--- client/AboutScreen.cpp
+++ client/AboutScreen.cpp
@@ -3,22 +3,26 @@
 
 #include "AboutScreen.h"
 #include "GladeLoader.h"
+#include "MainWindow.h"
+#include "SignalProtocol.h"
 
-AboutScreen::AboutScreen(MainWindow& main_w) :
+AboutScreen::AboutScreen(MainWindow* main_w) :
 		main_w(main_w) {
 	GladeLoader::ScreenBuilder builder = GladeLoader::load_glade_file(
 			"view/about.glade", this);
 	Gtk::Button* btn = NULL;
+	builder->get_widget("back_btn", btn);
 	if (btn) {
 		btn->signal_clicked().connect(
 				sigc::mem_fun(*this, &AboutScreen::on_exit_btn_clicked));
 	}
 
-	show_all_children();
+	this->show();
+	//show_all_children();
 }
 
 void AboutScreen::on_exit_btn_clicked() {
-	signal_hide();
+	hide();
+	main_w->process_signal(ABOUT_BACK);
 }
-AboutScreen::~AboutScreen() {
-}
+AboutScreen::~AboutScreen() {}
