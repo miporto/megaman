0ca474cc5575821120974f7449efff5207cba3cc
mporto <manu.porto94@gmail.com>
2016-05-29 20:11:46 -0300

    Add support for more events

--- client/view/InputHandler.cpp
+++ client/view/InputHandler.cpp
@@ -1,16 +1,29 @@
 #include "InputHandler.h"
 
-InputHandler::InputHandler() : window_closed(false) {}
+InputHandler::InputHandler() : window_closed(false) { }
 
 void InputHandler::read_input() {
     if (SDL_PollEvent(&event)) {
         if (event.type == SDL_QUIT) {
             window_closed = true;
+        } else if (event.type == SDL_KEYDOWN) {
+            switch (event.key.keysym.sym) {
+                case SDLK_RIGHT:
+                    break;
+            }
+        } else if (event.type == SDL_KEYUP) {
+            switch (event.key.keysym.sym) {
+                case SDLK_RIGHT:
+                    // set right to false
+                    break;
+            }
+
         }
     }
 }
+
 bool InputHandler::is_window_closed() {
     return window_closed;
 }
 
-InputHandler::~InputHandler() {}
+InputHandler::~InputHandler() { }
