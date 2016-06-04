7db1806f29efefaf38cea497344ecaa51954175e
mporto <manu.porto94@gmail.com>
2016-06-04 19:11:19 -0300

    Initial version of the TickInfoParser, not yet tested

--- client/TickInfoParser.h
+++ client/TickInfoParser.h
@@ -5,13 +5,18 @@
 #include <string>
 #include <vector>
 
+typedef std::vector<std::vector<int>> Coordinates;
+typedef std::map<std::string, Coordinates> TickParserInfo;
+
 class TickInfoParser {
 public:
     explicit TickInfoParser(const std::string& tick_info);
+    TickParserInfo get_parsed_tick_info();
     ~TickInfoParser();
 private:
-    typedef std::vector<std::vector<int>> Coordinates;
-    typedef std::map<std::string, std::vector<Coordinates>> TickInfoMap;
+    Coordinates coordinates_of(const std::string& actor);
+    std::string tick_info;
+    TickParserInfo tick_parser_info;
 };
 
 #endif //TICKINFOPARSER_H
