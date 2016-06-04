546a36b916ecbd5cf6a76b546e9529ec5c6ec20d
mporto <manu.porto94@gmail.com>
2016-06-04 02:08:59 -0300

    Change sprite sheet source

--- client/view/MegamanRenderer.cpp
+++ client/view/MegamanRenderer.cpp
@@ -4,14 +4,13 @@
 
 MegamanRenderer::MegamanRenderer(SDL2pp::Renderer* renderer) :
         renderer(renderer), run_phase(-1) {
-    sprites = new SDL2pp::Texture(*renderer, "resources/M484SpaceSoldier"
-            ".png");
+    sprites = new SDL2pp::Texture(*renderer, "resources/8bitmegaman.png");
 }
 
 void MegamanRenderer::render(int dest_x, int dest_y) {
     // TODO: request more info to be able to draw the proper sprite
     renderer->Copy(*sprites,
-                   SDL2pp::Rect(8, 11, 50, 50),
+                   SDL2pp::Rect(127, 286, 32, 28),
                    SDL2pp::Rect(dest_x, dest_y - 50, 50, 50));
 }
 
