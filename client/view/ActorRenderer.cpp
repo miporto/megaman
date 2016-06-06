06951b1f4afa5178878e53a2e7d2c10ae574efbe
mporto <manu.porto94@gmail.com>
2016-06-06 20:18:27 -0300

    Add rendering for megaman

--- client/view/ActorRenderer.cpp
+++ client/view/ActorRenderer.cpp
@@ -2,13 +2,21 @@
 
 ActorRenderer::ActorRenderer(SDL2pp::Renderer *renderer) : renderer(renderer) {
     renderers["Met"] = &ActorRenderer::renderMet;
+    renderers["Megaman"] = &ActorRenderer::render_megaman;
     sprites = new SDL2pp::Texture(*renderer, "resources/mm3_enemysheet.png");
+    megaman_sprites = new SDL2pp::Texture(*renderer, "resources/8bitmegaman"
+            ".png");
 }
 
 ActorRenderers ActorRenderer::get_renderers() {
     return renderers;
 }
 
+void ActorRenderer::render_megaman(int dest_x, int dest_y, int dir_x,
+                                   int dir_y) {
+    renderer->Copy(*megaman_sprites, SDL2pp::Rect(127, 286, 32, 28),
+               SDL2pp::Rect(dest_x, dest_y - 50, 50, 50));
+}
 void ActorRenderer::renderMet(int dest_x, int dest_y, int dir_x, int dir_y) {
     // TODO: put correct coordinates to the met sprite
     renderer->Copy(*sprites,
--- client/view/ActorRenderer.h
+++ client/view/ActorRenderer.h
@@ -14,12 +14,14 @@ class ActorRenderer {
 public:
     explicit ActorRenderer(SDL2pp::Renderer* renderer);
     ActorRenderers get_renderers();
+    void render_megaman(int dest_x, int dest_y, int dir_x, int dir_y);
     void renderMet(int dest_x, int dest_y, int dir_x, int dir_y);
     virtual ~ActorRenderer();
 private:
     ActorRenderers renderers;
     SDL2pp::Renderer* renderer;
     SDL2pp::Texture* sprites;
+    SDL2pp::Texture* megaman_sprites;
 };
 
 
