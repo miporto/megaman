3769ff43d5d7db4d70961142dc5d742fd36fa36e
manuporto <manu.porto94@gmail.com>
2016-06-07 15:25:42 -0300

    Test floats in json string

--- examples/json_ex/json_test.cpp
+++ examples/json_ex/json_test.cpp
@@ -6,12 +6,15 @@
 using json = nlohmann::json;
 
 int main(){
-    std::string file_dump;
-    std::string buffer;
-    std::ifstream json_file("example.json");
-    while (getline(json_file, buffer)) file_dump += buffer;
-    json j = json::parse(file_dump);
-    std::cout << j["list"][0]["x"] << j["list"][1]["y"] << std::endl;
+//    std::string file_dump;
+//    std::string buffer;
+//    std::ifstream json_file("example.json");
+//    while (getline(json_file, buffer)) file_dump += buffer;
+//    json j = json::parse(file_dump);
+//    std::cout << j["list"][0]["x"] << j["list"][1]["y"] << std::endl;
 
+    std::string test = "{\"object\":{\"Block\":[{\"x\":10,\"y\":11.7}]}}";
+    json j = json::parse(test);
+    std::cout << j.dump() << std::endl;
     return 0;
 }
