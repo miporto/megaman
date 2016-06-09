72a39cc4feecb688c226f96bd236685d3a753f31
mporto <manu.porto94@gmail.com>
2016-06-09 16:34:04 -0300

    Add new actor renderers classes

--- client/view/ActorRenderer.cpp
+++ client/view/ActorRenderer.cpp
@@ -1,11 +1,14 @@
+#include <string>
 #include "ActorRenderer.h"
 
 ActorRendererr::ActorRendererr(SDL2pp::Renderer *renderer,
-                               SDL2pp::Texture *sprites, int pos_x, int pos_y) :
+                               SDL2pp::Texture *sprites, float pos_x, float
+                               pos_y) :
         renderer(renderer), sprites(sprites), pos_x(pos_x), pos_y(pos_y),
         dir_x(0), dir_y(0) { }
 
-void ActorRendererr::update(int pos_x, int pos_y, int dir_x, int dir_y) {
+void ActorRendererr::update(float pos_x, float pos_y, int dir_x, int
+dir_y) {
     this->pos_x = pos_x;
     this->pos_y = pos_y;
     this->dir_x = dir_x;
@@ -13,12 +16,50 @@ void ActorRendererr::update(int pos_x, int pos_y, int dir_x, int dir_y) {
 }
 
 void MetRenderer::render() {
+    float y = pos_y * 50 + 50;
     renderer->Copy(*sprites,
                    SDL2pp::Rect(57, 17, 18, 19),
-                   SDL2pp::Rect(pos_x * 50, renderer->GetOutputHeight() -
-                                            pos_y * 50 - 50, 50, 50));
+                   SDL2pp::Rect((int) pos_x * 50, renderer->GetOutputHeight() -
+                                                  (int) y, 50, 50));
 }
 
+void MegaManRenderer::render() {
+    float y = pos_y * 50 + 50;
+    renderer->Copy(*sprites, SDL2pp::Rect(103, 10, 32, 28),
+                   SDL2pp::Rect((int) pos_x * 50,
+                                renderer->GetOutputHeight() -
+                                (int) y, 50, 50));
+}
+
+ActorRendererFactory::ActorRendererFactory(SDL2pp::Renderer *renderer) :
+        renderer(renderer){
+    sprites = new SDL2pp::Texture(*renderer, "resources/8bitmegaman."
+            "png");
+    actor_renderers["Met"] = MET_R;
+    actor_renderers["MegaMan"] = MEGAMAN_R;
+}
+
+ActorRendererr *ActorRendererFactory::build_actor_renderer(std::string
+                                                           tile_type,
+                                                           float pos_x, float
+                                                           pos_y) {
+    ActorRendererr *actor_renderer = NULL;
+    ActorRendererType tile_renderer_id = actor_renderers[tile_type];
+    switch (tile_renderer_id) {
+        case MET_R:
+            actor_renderer = new MetRenderer(renderer, sprites, pos_x, pos_y);
+            break;
+        case MEGAMAN_R:
+            actor_renderer = new MegaManRenderer(renderer, sprites, pos_x,
+                                                pos_y);
+            break;
+        default:
+            throw "ERROR: Non-existint actor renderer!";
+    }
+    return actor_renderer;
+}
+
+// Old stuff
 ActorRenderer::ActorRenderer(SDL2pp::Renderer *renderer) : renderer(renderer) {
     renderers["Met"] = &ActorRenderer::renderMet;
     renderers["MegaMan"] = &ActorRenderer::render_megaman;
@@ -35,28 +76,32 @@ ActorRenderers ActorRenderer::get_renderers() {
 void ActorRenderer::render_megaman(int dest_x, int dest_y, int dir_x,
                                    int dir_y) {
     renderer->Copy(*megaman_sprites, SDL2pp::Rect(103, 10, 32, 28),
-               SDL2pp::Rect(dest_x + dest_x * 50, renderer->GetOutputHeight() -
-                       dest_y*50 -
-                       50, 50, 50));
+                   SDL2pp::Rect(dest_x + dest_x * 50,
+                                renderer->GetOutputHeight() -
+                                dest_y * 50 -
+                                50, 50, 50));
 //    renderer->Copy(*megaman_sprites, SDL2pp::Rect(103, 10, 32, 28),
 //               SDL2pp::Rect(300, 300 - 50, 50, 50));
 }
+
 void ActorRenderer::renderMet(int dest_x, int dest_y, int dir_x, int dir_y) {
     // TODO: put correct coordinates to the met sprite
     renderer->Copy(*sprites,
                    SDL2pp::Rect(57, 17, 18, 19),
-                   SDL2pp::Rect(dest_x + dest_x*50, renderer->GetOutputHeight
-                           () -
-                           dest_y*50 - 50, 50, 50));
+                   SDL2pp::Rect(dest_x + dest_x * 50, renderer->GetOutputHeight
+                                        () -
+                                                      dest_y * 50 - 50, 50,
+                                50));
 }
 
 void ActorRenderer::render_pellet(int dest_x, int dest_y, int dir_x,
                                   int dir_y) {
-    SDL_Rect fillRect = {dest_x + dest_x*50, renderer->GetOutputHeight() -
-            dest_y - 50, 5, 5};
+    SDL_Rect fillRect = {dest_x + dest_x * 50, renderer->GetOutputHeight() -
+                                               dest_y - 50, 5, 5};
     SDL_SetRenderDrawColor(renderer->Get(), 0xFF, 0x00, 0x00, 0xFF);
     SDL_RenderFillRect(renderer->Get(), &fillRect);
 }
+
 ActorRenderer::~ActorRenderer() {
     delete sprites;
 }
--- client/view/ActorRenderer.h
+++ client/view/ActorRenderer.h
@@ -6,19 +6,16 @@
 #include <string>
 #include <vector>
 
-
-class ActorRenderer;
-
-typedef void (ActorRenderer::*arenderer_method_t)(int, int, int, int);
-
-typedef std::map<std::string, arenderer_method_t> ActorRenderers;
-
+enum ActorRendererType {
+    MET_R,
+    MEGAMAN_R
+};
 class ActorRendererr {
 public:
     ActorRendererr(SDL2pp::Renderer *renderer, SDL2pp::Texture *sprites,
-                   int pos_x, int pos_y);
+                   float pos_x, float pos_y);
 
-    void update(int pos_x, int pos_y, int dir_x, int dir_y);
+    void update(float pos_x, float pos_y, int dir_x, int dir_y);
 
     virtual void render() = 0;
 
@@ -27,18 +24,40 @@ public:
 protected:
     SDL2pp::Renderer *renderer;
     SDL2pp::Texture *sprites;
-    int pos_x;
-    int pos_y;
+    float pos_x;
+    float pos_y;
     int dir_x;
     int dir_y;
 };
 
 class MetRenderer : public ActorRendererr {
-    MetRenderer(SDL2pp::Renderer *renderer, SDL2pp::Texture *sprites, int pos_x,
-                int pos_y);
-
-    virtual void render();
+public:
+    using ActorRendererr::ActorRendererr;
+    void render();
 };
+
+class MegaManRenderer : public ActorRendererr {
+public:
+    using ActorRendererr::ActorRendererr;
+    void render();
+};
+
+class ActorRendererFactory {
+public:
+    explicit ActorRendererFactory(SDL2pp::Renderer * renderer);
+    ActorRendererr* build_actor_renderer(std::string tile_type, float pos_x,
+                                       float pos_y);
+    virtual ~ActorRendererFactory() {}
+private:
+    SDL2pp::Renderer *renderer;
+    SDL2pp::Texture *sprites;
+    std::map<std::string, ActorRendererType> actor_renderers;
+};
+class ActorRenderer;
+
+typedef void (ActorRenderer::*arenderer_method_t)(int, int, int, int);
+
+typedef std::map<std::string, arenderer_method_t> ActorRenderers;
 
 class ActorRenderer {
 public:
