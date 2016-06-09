85b89c63db8cb60a51cc2aa99a26eef770949310
mporto <manu.porto94@gmail.com>
2016-06-09 15:42:33 -0300

    Dont delete old packets, use them

--- client/communication/ClientCommunicator.cpp
+++ client/communication/ClientCommunicator.cpp
@@ -43,9 +43,6 @@ void ClientCommunicator::send_stage_pick(char& stage_id) {
 const std::string ClientCommunicator::receive_stage_info() {
     if (!this->packets_received.is_empty(STAGE_INFO)) {
         StageInfo* stage = (StageInfo*)this->packets_received.pop(STAGE_INFO);
-        while (!packets_received.is_empty(STAGE_INFO)) {
-            stage = (StageInfo*) packets_received.pop(STAGE_INFO);
-        }
         std::string info = stage->get_info();
         delete stage;
         return info;
