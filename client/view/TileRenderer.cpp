e3059e570b5b5dc250b9bc4073216ac1c2a2fe77
mporto <manu.porto94@gmail.com>
2016-06-10 00:27:59 -0300

    Remove unused classes and old methods

--- client/view/TileRenderer.cpp
+++ client/view/TileRenderer.cpp
@@ -60,34 +60,3 @@ TileRendererr* TileRendererFactory::build_tile_renderer(std::string tile_type,
     }
     return tile_renderer;
 }
-// Old stuff
-TileRenderer::TileRenderer(SDL2pp::Renderer *renderer) :
-        renderer(renderer) {
-    // TODO: be coherent with identifiers of json file
-    renderers["Block"] = &TileRenderer::renderBlock;
-    renderers["Stairs"] = &TileRenderer::renderStairs;
-    sprites = new SDL2pp::Texture(*renderer, "resources/mm3_8boss_shadowman."
-            "png");
-}
-
-TileRenderers TileRenderer::get_renderers() {
-    return renderers;
-}
-
-void TileRenderer::renderBlock(int dest_x, int dest_y) {
-    renderer->Copy(*sprites,
-                   SDL2pp::Rect(52, 119, 16, 16),
-                   SDL2pp::Rect(dest_x * 50, renderer->GetOutputHeight()
-                                             - dest_y * 50 - 50, 50, 50));
-}
-
-void TileRenderer::renderStairs(int dest_x, int dest_y) {
-    renderer->Copy(*sprites,
-                   SDL2pp::Rect(35, 85, 16, 16),
-                   SDL2pp::Rect(dest_x * 50, renderer->GetOutputHeight()
-                                             - dest_y * 50 - 50, 50, 50));
-}
-
-TileRenderer::~TileRenderer() {
-    delete sprites;
-}
--- client/view/TileRenderer.h
+++ client/view/TileRenderer.h
@@ -61,32 +61,4 @@ private:
     std::map<std::string, TileRendererType> tile_renderers;
 };
 
-
-
-// Old stuff, deprecated --siempre quise usar esta palabra (?
-class TileRenderer;
-
-typedef void (TileRenderer::*trenderer_method_t)(int, int);
-
-typedef std::map<std::string, trenderer_method_t> TileRenderers;
-
-class TileRenderer {
-public:
-    explicit TileRenderer(SDL2pp::Renderer *renderer);
-
-    TileRenderers get_renderers();
-
-    void renderBlock(int dest_x, int dest_y);
-
-    void renderStairs(int dest_x, int dest_y);
-
-    virtual ~TileRenderer();
-
-private:
-    TileRenderers renderers;
-    SDL2pp::Renderer *renderer;
-    SDL2pp::Texture *sprites;
-};
-
-
 #endif //MEGAMAN_BLOCKRENDERER_H
