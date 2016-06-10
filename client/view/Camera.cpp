bbbfcab4febecac1dc08b3f2b612f85aca0a68c5
mporto <mporto@fi.uba.ar>
2016-06-10 13:46:38 -0300

    Add adjust position method to camera

--- client/view/Camera.cpp
+++ client/view/Camera.cpp
@@ -3,4 +3,9 @@
 Camera::Camera(SDL2pp::Renderer *renderer) : renderer(renderer), offset_x(0),
                                              offset_y(0) { }
 
+AdjustedPos Camera::adjust_position(float x, float y) {
+    int adj_x = (int) x*50 - offset_x;
+    int adj_y = renderer->GetOutputHeight() - (int) (y + 1)*50 + offset_y;
+    return AdjustedPos(adj_x, adj_y);
+}
 Camera::~Camera() {}
--- client/view/Camera.h
+++ client/view/Camera.h
@@ -1,12 +1,14 @@
 #ifndef MEGAMAN_CAMERA_H
 #define MEGAMAN_CAMERA_H
 
-
 #include <extern/libSDL2pp/SDL2pp/Renderer.hh>
+#include <utility>
 
+typedef std::pair<int, int> AdjustedPos;
 class Camera {
 public:
     explicit Camera(SDL2pp::Renderer *renderer);
+    AdjustedPos adjust_position(float x, float y);
     ~Camera();
 private:
     SDL2pp::Renderer *renderer;
