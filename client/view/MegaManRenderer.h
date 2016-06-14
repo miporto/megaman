327ea935fc834bc704999552b8019f18a532ad37
mporto <manu.porto94@gmail.com>
2016-06-14 12:01:20 -0300

    Add new Mega Man renderer

--- client/view/MegaManRenderer.h
+++ client/view/MegaManRenderer.h
@@ -2,8 +2,34 @@
 #define MEGAMAN_MEGAMANRENDERER_H
 
 
-class MegaManRenderer {
+#include <extern/libSDL2pp/SDL2pp/Renderer.hh>
+#include <string>
 
+#include "Camera.h"
+
+class Camera;
+class MMegaManRenderer {
+public:
+    MMegaManRenderer(SDL2pp::Renderer *renderer, Camera &camera, float pos_x,
+                     float pos_y, int energy, const std::string &name);
+    void update(float pos_x, float pos_y, int dir_x, int dir_y, int energy);
+    void render();
+    float get_x();
+    float get_y();
+    ~MMegaManRenderer();
+private:
+    void render_energy();
+    void render_name();
+    SDL2pp::Renderer *renderer;
+    SDL2pp::Texture *sprites;
+    Camera &camera;
+    float pos_x;
+    float pos_y;
+    int dir_x;
+    int dir_y;
+    int initial_energy;
+    int actual_energy;
+    const std::string &name;
 };
 
 
