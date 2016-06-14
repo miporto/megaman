ed457812188b71bcdaaa3b41c8a096912ebe5e90
mporto <manu.porto94@gmail.com>
2016-06-14 08:46:47 -0300

    Handle remaining inputs (weapons change and goind down)

--- client/view/InputHandler.cpp
+++ client/view/InputHandler.cpp
@@ -5,7 +5,7 @@
 #include "InputHandler.h"
 #include "common/communication/Packet.h"
 
-InputHandler::InputHandler() : window_closed(false), keys_held(4) {
+InputHandler::InputHandler() : window_closed(false), keys_held(11) {
     for (size_t i = 0; i < keys_held.size(); ++i) {
         keys_held[i] = false;
     }
@@ -26,6 +26,27 @@ void InputHandler::read_input() {
                 case SDLK_RIGHT:
                     keys_held[RIGHT-1] = true;
                     break;
+                case SDLK_DOWN:
+                    keys_held[DOWN-1] = true;
+                    break;
+                case SDLK_0:
+                    keys_held[AMMO_0-1] = true;
+                    break;
+                case SDLK_1:
+                    keys_held[AMMO_1-1] = true;
+                    break;
+                case SDLK_2:
+                    keys_held[AMMO_2-1] = true;
+                    break;
+                case SDLK_3:
+                    keys_held[AMMO_3-1] = true;
+                    break;
+                case SDLK_4:
+                    keys_held[AMMO_4-1] = true;
+                    break;
+                case SDLK_5:
+                    keys_held[AMMO_5-1] = true;
+                    break;
                 case SDLK_s:
                     keys_held[SHOOT-1] = true;
                     break;
@@ -40,6 +61,27 @@ void InputHandler::read_input() {
                     break;
                 case SDLK_RIGHT:
                     keys_held[RIGHT-1] = false;
+                    break;
+                case SDLK_DOWN:
+                    keys_held[DOWN-1] = false;
+                    break;
+                case SDLK_0:
+                    keys_held[AMMO_0-1] = false;
+                    break;
+                case SDLK_1:
+                    keys_held[AMMO_1-1] = false;
+                    break;
+                case SDLK_2:
+                    keys_held[AMMO_2-1] = false;
+                    break;
+                case SDLK_3:
+                    keys_held[AMMO_3-1] = false;
+                    break;
+                case SDLK_4:
+                    keys_held[AMMO_4-1] = false;
+                    break;
+                case SDLK_5:
+                    keys_held[AMMO_5-1] = false;
                     break;
                 case SDLK_s:
                     keys_held[SHOOT-1] = false;
