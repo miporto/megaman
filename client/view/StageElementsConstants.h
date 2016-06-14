66f616403f33bed30244a86f37bd5642f7cf3b72
mporto <manu.porto94@gmail.com>
2016-06-14 02:01:43 -0300

    Add new boss renderer class family

---
+++ client/view/StageElementsConstants.h
@@ -0,0 +1,19 @@
+#ifndef MEGAMAN_STAGEELEMENTSCONSTANTS_H
+#define MEGAMAN_STAGEELEMENTSCONSTANTS_H
+
+#define BLOCK "Block"
+#define STAIRS "Stairs"
+#define SPIKE "Spike"
+#define CLIFF "Cliff"
+#define MEGAMAN "MegaMan"
+#define BOMBMAN "BombMan"
+#define MAGNETMAN "MagnetMan"
+#define SPARKMAN "SparkMan"
+#define RINGMAN "RingMan"
+#define FIREMAN "FireMan"
+#define MET "Met"
+#define BUMBY "Bumby"
+#define SNIPER "Sniper"
+#define JSNIPER "JumpingSniper"
+
+#endif //MEGAMAN_STAGEELEMENTSCONSTANTS_H
