49bbf1ff5763ff09deefc90f06866fb86140b030
Agustina Barbetta <agustina.barbetta@gmail.com>
2016-06-20 12:20:04 -0300

    ReceiverError fixed

--- server/communication/ServerReceiver.cpp
+++ server/communication/ServerReceiver.cpp
@@ -6,20 +6,23 @@ ServerReceiver::ServerReceiver(Socket* socket,
         : Receiver(socket, packets), events(events) {}
 
 void ServerReceiver::receive_packet(const char id) {
-    Receiver::receive_packet(id);
-    if (id == ACTION) {
-        int length;
-        this->socket->receive((char *) &length, sizeof(int));
-        char *name = new char[length + 1];
-        name[length] = '\0';
-        this->socket->receive(name, sizeof(char) * length);
-        char action_id, pressed;
-        this->socket->receive(&action_id, sizeof(char));
-        this->socket->receive(&pressed, sizeof(char));
-        this->events.push(new Action(name, action_id, pressed));
-        delete[] name;
-    } else {
-        throw ReceiverError("Unknown packet id");
+    try {
+        Receiver::receive_packet(id);
+    } catch (const ReceiverError& e) {
+        if (id == ACTION) {
+            int length;
+            this->socket->receive((char *) &length, sizeof(int));
+            char *name = new char[length + 1];
+            name[length] = '\0';
+            this->socket->receive(name, sizeof(char) * length);
+            char action_id, pressed;
+            this->socket->receive(&action_id, sizeof(char));
+            this->socket->receive(&pressed, sizeof(char));
+            this->events.push(new Action(name, action_id, pressed));
+            delete[] name;
+        } else {
+            throw;
+        }
     }
 }
 
